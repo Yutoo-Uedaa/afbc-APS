f73033939a1ef01ed24d6d76108494116443580c
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-06-09 15:50:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,14 @@ def inc_count():
 
 st.title('APS評価実験')
 
-if st.session_state.count == -2:
-    
-    explainBef = st.empty()
-    with explainBef.expander("実験の説明", True):
-        st.write(kyouji)
+st.set_page_config(layout="wide")
+#セレクトボックスのリストを作成
+pagelist = ["page1","page2"]
+#サイドバーのセレクトボックスを配置
+selector=st.sidebar.selectbox( "ページ選択",pagelist)
+if selector=="page1":
+    if st.button('ページ1ボタン'):
+        st.title("ページ1のタイトル")
+elif selector=="page2":
+    if st.button('ページ2ボタン'):
+        st.title("ページ2のタイトル")
