2c28f5823f1400d7421e13bef2b691d2321cec51
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-06-14 15:39:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ if st.session_state.count == -3:
     
     if st.button("終了"):
         # df.to_csv("output.csv",index = False,encoding = "utf_8_sig)
-        df.to_csv('out.csv', mode='a',
+        df.to_csv('data/out.csv', mode='a',
                       header=False, index=False,
                       encoding='utf_8_sig')
         st.write(select1,select2,select3)
