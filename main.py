01cdfca598ab4b0fb0d32089b7087732dedc56c8
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-06-15 11:53:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ if st.session_state.count == -1:
         sizi.empty()
         sizi2.empty()
         sizi3.empty()
-        st.session_state.group == 0
+        st.session_state.count == 0
     
-if st.session_state.group >= 0:    
+if st.session_state.count >= 0:    
     st.header('１．クラシック')
     st.write('リファレンス音源\n何回聞いても問題ありません')
     audio_file=open('Test1_reference_60_classics.wav','rb')
