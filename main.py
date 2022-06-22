942ae2045cf32a56464b4d1d1c56f9e2387e97f2
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-06-22 14:06:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -118,7 +118,8 @@ if st.session_state.count == 0:
     wine = pd.read_csv('data/outresult_sample6.csv', names=('name','source','  AFC(1)/FS-AFC(2)','  AFC(1)/Prop(2)','  FS-AFC(1)/Prop(2)'))
     st.dataframe(wine)
     data1=wine
-   data1=data1.query('source == "クラシック"')
+    st.dataframe(data1)
+    data1=data1.query('source == "クラシック"')
     
     
     csv_financde = wine.to_csv().encode('utf-8-sig').decode()
