2102f28ecaefd21c4eaf6bd8af94caea8af57be8
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-06-27 14:56:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ if st.session_state.count == 0:
     s_bool1 = ((data1['source'] == 'ドアベル') & (data1['AFC(1)/FS-AFC(2)'] == 2))
     s_bool2 = ((data1['source'] == 'ドアベル') & (data1['AFC(1)/Prop(2)'] == 2))
     s_bool3 = ((data1['source'] == 'ドアベル') & (data1['FS-AFC(1)/Prop(2)'] == 2))
-    df4 = pd.DataFrame({'1': ['鐘の音','AFC/FS-AFC','AFC/Prop','FS-AFC/Prop'], '2': ['AFC',str(sum_all.sum()-s_bool1.sum()),str(sum_all.sum()-s_bool2.sum()),'0'],
+    df4 = pd.DataFrame({'1': ['ドアベル','AFC/FS-AFC','AFC/Prop','FS-AFC/Prop'], '2': ['AFC',str(sum_all.sum()-s_bool1.sum()),str(sum_all.sum()-s_bool2.sum()),'0'],
                        '3': ['FS',str(s_bool1.sum()),'0',str(sum_all.sum()-s_bool3.sum())], '4': ['Prop','0',str(s_bool2.sum()),str(s_bool3.sum())]})
     #st.dataframe(df4)
     data1=wine.query('source == "電話のコール音"')
