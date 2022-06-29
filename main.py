2182f215873649a480aaa83f3066bb4b532d956b
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-06-29 11:32:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -236,9 +236,8 @@ if st.session_state.count == -19:
     st.select1_3=select3 
       
     nex=st.empty()
-    delay(30)
-    next1_button=nex.button('次の音源へ(2/18)')
-    if next1_button:
+    #next1_button=
+    if nex.button('次の音源へ(2/18)')
         sizi.empty()
         sizi2.empty()
         sizi3.empty()
