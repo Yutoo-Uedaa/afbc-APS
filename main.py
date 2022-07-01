6be383eb9932b73eb90891f0fa3d8f93112b5753
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-07-01 15:12:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -119,9 +119,9 @@ if st.session_state.count == 0:
     wine = pd.read_csv('data/outresult_test3.csv', names=('name','source','AFC(1)/FS-AFC(2)','AFC(1)/Prop(2)','FS-AFC(1)/Prop(2)'))
     st.dataframe(wine)
     wine1=wine[(wine['AFC(1)/FS-AFC(2)'] != "value") & (wine['AFC(1)/Prop(2)'] != "value") & (wine['FS-AFC(1)/Prop(2)'] != "value")]
-    sum_all=((wine['source']=='クラシック'))
+    sum_all=((wine1['source']=='クラシック'))
     st.write('アンケート回答数 : '+ str(sum_all.sum()))
-    data1=wine1.query('source == "クラシック"')
+    data1=wine1.query('source == 'クラシック'' )
     s_bool1 = ((data1['source'] == 'クラシック') & (data1['AFC(1)/FS-AFC(2)'] == 2))
     s_bool2 = ((data1['source'] == 'クラシック') & (data1['AFC(1)/Prop(2)'] == 2))
     s_bool3 = ((data1['source'] == 'クラシック') & (data1['FS-AFC(1)/Prop(2)'] == 2))
