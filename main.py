a27592c6920eb429fd70d44d493381fb28b7efcc
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-07-08 13:13:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ if st.session_state.count == -21:
         st.session_state.count =0
     
 if st.session_state.count == 0:    
-    wine = pd.read_csv('data/outresult_pretest2.csv', names=('name','source','AFC(1)/FS-AFC(2)','AFC(1)/Prop(2)','FS-AFC(1)/Prop(2)'))
+    wine = pd.read_csv('data/outresult_TEST1.csv', names=('name','source','AFC(1)/FS-AFC(2)','AFC(1)/Prop(2)','FS-AFC(1)/Prop(2)'))
     st.dataframe(wine)
     wine1=wine[(wine['AFC(1)/FS-AFC(2)'] != "value") & (wine['AFC(1)/Prop(2)'] != "value") & (wine['FS-AFC(1)/Prop(2)'] != "value")]
     sum_all=((wine1['source']=='クラシック'))
@@ -1063,7 +1063,7 @@ if st.session_state.count == -1:
                        'FS-AFC(1)/Prop(2)': [st.select1_3,st.select2_3,st.select3_3,st.select4_3,st.select5_3,st.select6_3]
                       })    
     st.dataframe(df)
-    df.to_csv('data/outresult_pretest2.csv', mode='a',header=False, index=False,encoding='utf_8_sig')
+    df.to_csv('data/outresult_TEST1.csv', mode='a',header=False, index=False,encoding='utf_8_sig')
     st.write('結果が"value"の場合はお手数ですがもう一度お願いします。')
     
       
