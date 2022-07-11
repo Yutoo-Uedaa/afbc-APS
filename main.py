5c02c55c92451d45cb09ee6a41133d02ed29536f
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-07-11 13:27:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1026,15 +1026,6 @@ if st.session_state.count == -2:
     else:
         st.select6_1=str('1')
     
-    explain=st.empty()    
-    with explain.form("my_form3"):
-        st.write('今回のアンケートで気づいたことなどありましたらコメントお願いします。')
-        text = st.text_input(label='記入欄', value='')
-        if st.form_submit_button("送信"):
-            dff=pd.DataFrame({'名前':[st.session_state.key],
-                              'コメント':[text]
-                             })
-            dff.to_csv('data/coment1.csv', mode='a',header=False, index=False,encoding='utf_8_sig')
     
     nex=st.empty()
     next1_button=nex.button('終了')
