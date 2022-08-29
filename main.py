c57139f4f702b93bd35c6e41e470b2141efc270f
Yutoo-Uedaa <82076429+Yutoo-Uedaa@users.noreply.github.com>
2022-08-29 13:27:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -672,7 +672,7 @@ if st.session_state.count == -28:
     my_bar = st.progress(0)
     my_bar.progress(int(100/36*10))
     sizi = st.empty()
-    sizi.header('１ー５．クラシック')
+    sizi.header('２ー５．鐘の音')
     # リファレンス音源
     sizi2 = st.empty()
     sizi2.write('・リファレンス音源')
@@ -1750,7 +1750,7 @@ if st.session_state.count == -4:
 # 英語の音声6-6 sw/Prop　
 if st.session_state.count == -3:
     my_bar = st.progress(0)
-    my_bar.progress(int(100/36*3))
+    my_bar.progress(int(100/36*36))
     sizi = st.empty()
     sizi.header('６ー６．英語の音声')
     # リファレンス音源
@@ -1801,8 +1801,11 @@ if st.session_state.count == -1:
     df = pd.DataFrame({'名前': [st.session_state.key,st.session_state.key,st.session_state.key,st.session_state.key,st.session_state.key,st.session_state.key], 
                        '音声': ['クラシック','鐘の音','救急車のサイレン','ドアベル','電話のコール音','英語の音声'], 
                        'AFC(1)/FS-AFC(2)': [st.select1_1,st.select2_1,st.select3_1,st.select4_1,st.select5_1,st.select6_1],
-                       'AFC(1)/Prop(2)': [st.select1_2,st.select2_2,st.select3_2,st.select4_2,st.select5_2,st.select6_2], 
-                       'FS-AFC(1)/Prop(2)': [st.select1_3,st.select2_3,st.select3_3,st.select4_3,st.select5_3,st.select6_3]
+                       'AFC(1)/swPEM-AFC(2)': [st.select1_2,st.select2_2,st.select3_2,st.select4_2,st.select5_2,st.select6_2],
+                       'AFC(1)/Prop(2)': [st.select1_3,st.select2_3,st.select3_3,st.select4_3,st.select5_3,st.select6_3], 
+                       'FS-AFC(1)/swPEM-AFC(2)': [st.select1_4,st.select2_4,st.select3_4,st.select4_4,st.select5_4,st.select6_4],
+                       'FS-AFC(1)/Prop(2)': [st.select1_5,st.select2_5,st.select3_5,st.select4_5,st.select5_5,st.select6_5],
+                       'swPEM-AFC(1)/Prop(2)': [st.select1_6,st.select2_6,st.select3_6,st.select4_6,st.select5_6,st.select6_6],
                       })    
     st.dataframe(df)
     df.to_csv('data/outresult_TEST1.csv', mode='a',header=False, index=False,encoding='utf_8_sig')
